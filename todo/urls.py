4e0d940a73fee2acbd35dbe3d0581920ce73fbaa
tito <juffigueroama@unal.edu.co>
2023-05-09 22:39:37 -0500

    add and delete now are using ajax

--- todo/urls.py
+++ todo/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('task_create_modal/', views.task_create_modal, name='task_create_modal'),
     path('completed/', views.completed_tasks, name='completed_tasks'),
+    path('undocomplete/', views.undo_completed_tasks, name='undo_completed_tasks'),
     path('new/', views.task_create, name='task_create'),
     path('edit/<int:pk>/', views.task_edit, name='task_edit'),
     path('delete/<int:pk>/', views.task_delete, name='task_delete'),
--- todo/views.py
+++ todo/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponseBadRequest,HttpResponse
 from .models import Task
 from .forms import TaskForm
 from django.core.serializers import serialize
+from django.core.serializers.json import Serializer as JsonSerializer
 from django.views.decorators.csrf import csrf_exempt
 # def task_list(request):
 #     tasks = Task.objects.all().order_by('due_date', 'priority')
@@ -14,10 +15,20 @@ def task_list(request):
     tasks = Task.objects.filter(completed=False).order_by('due_date', 'priority')
     return render(request, 'todo/task_list.html', {'tasks': tasks})
 
+class TaskSerializer(JsonSerializer):
+    def get_dump_object(self, obj):
+        dump_object = super().get_dump_object(obj)
+        dump_object['id'] = obj.pk
+        return dump_object
 def get_ordered_tasks(request):
-    tasks = Task.objects.filter(completed=False).order_by('due_date', ('priority'))
+    tasks = Task.objects.filter(completed=False).order_by('due_date', 'priority')
     serialized_tasks = serialize('json', tasks)
     return JsonResponse(serialized_tasks, safe=False)
+
+# def get_ordered_tasks(request):
+#     tasks = Task.objects.filter(completed=False).order_by('due_date', ('priority'))
+#     serialized_tasks = TaskSerializer().serialize(tasks)
+#     return JsonResponse(serialized_tasks, safe=False)
 
 def task_detail(request, pk):
     task = get_object_or_404(Task, pk=pk)
@@ -102,4 +113,17 @@ def completed_tasks(request):
     elif request.method =='GET':
         completed_tasks = Task.objects.filter(completed=True).order_by('due_date', 'priority')
         return render(request, 'todo/completed_tasks.html', {'completed_tasks': completed_tasks})
+    
+def undo_completed_tasks(request):
+    if request.method == 'POST':
+        task_id = request.POST.get('task_id')
+        task = Task.objects.get(id=task_id)
+        task.completed = False
+        task.save()
+        completed_tasks = Task.objects.filter(completed=True).order_by('due_date', 'priority')
+        return render(request, 'todo/completed_tasks.html', {'completed_tasks': completed_tasks})
+    elif request.method =='GET':
+        completed_tasks = Task.objects.filter(completed=True).order_by('due_date', 'priority')
+        return render(request, 'todo/completed_tasks.html', {'completed_tasks': completed_tasks})
+
 
